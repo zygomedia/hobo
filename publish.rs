46c44204ac834ed0ab28fdbbfb64e250ce0dc40e
Awpteamoose <awpteamoose@gmail.com>
2025-05-08 15:13:54 +0100

    cargo publish now waits on its own

--- publish.rs
+++ publish.rs
@@ -1,15 +1,10 @@
 //# execute = "*"
 
-use std::thread::sleep;
-use std::time::Duration;
 use execute::command as cmd;
 
 fn main() {
 	assert!(cmd!("cargo publish").current_dir("hobo_derive").status().unwrap().success());
-	sleep(Duration::from_secs(60));
 	assert!(cmd!("cargo publish").current_dir("hobo_css_macros").status().unwrap().success());
-	sleep(Duration::from_secs(60));
 	assert!(cmd!("cargo publish").current_dir("hobo_css").status().unwrap().success());
-	sleep(Duration::from_secs(60));
 	assert!(cmd!("cargo publish").current_dir("hobo").status().unwrap().success());
 }
